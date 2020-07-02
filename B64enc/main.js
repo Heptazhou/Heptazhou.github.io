25b3bdc786d3a37bc48bb004cfcf0ceb57929e78
Heptazhou <heptazhou@icloud.com>
2020-07-02 21:17:47 +0000

    Update

--- B64enc/main.js
+++ B64enc/main.js
@@ -1,33 +1,33 @@
 /* https://github.com/emn178/online-tools */
-(function ($, window, document, undefined) {
-	window.method = Base64.encode;
+;(function ($, window, document, undefined) {
+	window.method = Base64.encode
 	function copy_output() {
-		var copyText = document.querySelector("#output");
-		copyText.select();
-		document.execCommand("copy");
+		var copyText = document.querySelector("#output")
+		copyText.select()
+		document.execCommand("copy")
 	}
-	document.querySelector("#output").addEventListener("click", copy_output);
+	document.querySelector("#output").addEventListener("click", copy_output)
 	$(document).ready(function () {
-		var input = $("#input");
-		var output = $("#output");
-		var checkbox = $("#auto-update");
-		var option = $("[data-option]");
+		var input = $("#input")
+		var output = $("#output")
+		var checkbox = $("#auto-update")
+		var option = $("[data-option]")
 		var execute = function () {
 			try {
-				output.val(method(input.val(), option.val()).replace(/=*$/, "").replace(/\+/g, "-").replace(/\//g, "_"));
+				output.val(method(input.val(), option.val()).replace(/=*$/, "").replace(/\+/g, "-").replace(/\//g, "_"))
 			} catch (e) {
-				output.val(e);
+				output.val(e)
 			}
-		};
+		}
 		function autoUpdate() {
-			if (!checkbox[0].checked) return;
-			execute();
+			if (!checkbox[0].checked) return
+			execute()
 		}
 		if (checkbox.length > 0) {
-			input.bind("input propertychange", autoUpdate);
-			option.bind("input propertychange", autoUpdate);
-			checkbox.click(autoUpdate);
+			input.bind("input propertychange", autoUpdate)
+			option.bind("input propertychange", autoUpdate)
+			checkbox.click(autoUpdate)
 		}
-		$("#execute").click(execute);
-	});
-})(jQuery, window, document);
+		$("#execute").click(execute)
+	})
+})(jQuery, window, document)
--- main.js
+++ main.js
@@ -1,113 +1,113 @@
 function hashsplit(str) {
-	var list = str.match(/^#\/*(.*?)\/*$/);
-	if (list != null && list[1] != "") return list[1].split(/\/+/);
-	else return null;
+	var list = str.match(/^#\/*(.*?)\/*$/)
+	if (list != null && list[1] != "") return list[1].split(/\/+/)
+	else return null
 }
 function pdec(pointer) {
-	while (pointer.length % 4 != 0) pointer += "=";
-	return decodeURI(Base64.decode(pointer));
+	while (pointer.length % 4 != 0) pointer += "="
+	return decodeURI(Base64.decode(pointer))
 }
 function redirect(dest) {
-	console.log(dest);
-	document.body.innerHTML = "";
-	document.body.style = "margin: 2.7rem";
+	console.log(dest)
+	document.body.innerHTML = ""
+	document.body.style = "margin: 2.7rem"
 	if (quiet === false) {
-		document.body.innerHTML = `\n\t<h1 style="line-height: 3.14rem; font-weight: normal">Click to redirect...</h1>\n\t<br />\n\t<a href="${encodeURI(dest)}">> ${dest}</a>\n`;
+		document.body.innerHTML = `\n\t<h1 style="line-height: 3.14rem; font-weight: normal">Click to redirect...</h1>\n\t<br />\n\t<a href="${encodeURI(dest)}">> ${dest}</a>\n`
 	}
 	if (quiet === true) {
-		document.body.innerHTML = `\n\t<h1 style="line-height: 3.14rem; font-weight: normal">Redirecting...</h1>\n\t<br />\n\t<a href="${encodeURI(dest)}">> Click here if you are not redirected.</a>\n`;
-		location.hash = location.hash.replace(/^#\/*/, "#*/");
-		location.href = dest;
+		document.body.innerHTML = `\n\t<h1 style="line-height: 3.14rem; font-weight: normal">Redirecting...</h1>\n\t<br />\n\t<a href="${encodeURI(dest)}">> Click here if you are not redirected.</a>\n`
+		location.hash = location.hash.replace(/^#\/*/, "#*/")
+		location.href = dest
 	}
 }
 function hash_func() {
-	var list = hashsplit(location.hash);
-	if (list == null) return null;
+	var list = hashsplit(location.hash)
+	if (list == null) return null
 	for (var i = 0; i < list.length; i++) {
-		var para = list[i].replace(/=*$/, "").split("=");
+		var para = list[i].replace(/=*$/, "").split("=")
 		switch (para.length) {
 			case 1:
-				if (para[0] === "*") quiet = false;
-				break;
+				if (para[0] === "*") quiet = false
+				break
 			case 2:
-				var s = decodeURI(para[1].replace(/\*/g, "/"));
+				var s = decodeURI(para[1].replace(/\*/g, "/"))
 				switch (para[0]) {
 					case "url":
 						try {
-							s = pdec(s);
-							if (s.startsWith("//")) s = s.replace(/^\/+/, "https://");
-							redirect(s);
+							s = pdec(s)
+							if (s.startsWith("//")) s = s.replace(/^\/+/, "https://")
+							redirect(s)
 						} catch (e) {
-							console.log("Invalid pointer.");
+							console.log("Invalid pointer.")
 						}
-						break;
+						break
 					case "b":
-						redirect(`https://b23.tv/${s}`);
-						break;
+						redirect(`https://b23.tv/${s}`)
+						break
 					case "e":
 					case "eg":
-						redirect(`https://e-hentai.org/g/${s}`);
-						break;
+						redirect(`https://e-hentai.org/g/${s}`)
+						break
 					case "es":
-						redirect(`https://e-hentai.org/s/${s}`);
-						break;
+						redirect(`https://e-hentai.org/s/${s}`)
+						break
 					case "ew":
-						redirect(`https://ehwiki.org/index.php?title=Special:Search&fulltext=Search&search=${encodeURIComponent(s)}`);
-						break;
+						redirect(`https://ehwiki.org/index.php?title=Special:Search&fulltext=Search&search=${encodeURIComponent(s)}`)
+						break
 					case "g":
 					case "gp":
-						redirect(`https://gelbooru.com/index.php?page=post&s=view&id=${s}`);
-						break;
+						redirect(`https://gelbooru.com/index.php?page=post&s=view&id=${s}`)
+						break
 					case "k":
 					case "kp":
-						redirect(`https://konachan.com/post/show/${s}`);
-						break;
+						redirect(`https://konachan.com/post/show/${s}`)
+						break
 					case "n":
 						if (0);
-						else if (s.startsWith("sm")) redirect(`https://www.nicovideo.jp/watch/${s}`);
-						else if (s.startsWith("ch")) redirect(`https://ch.nicovideo.jp/channel/${s}`);
-						else if (s.startsWith("im")) redirect(`https://seiga.nicovideo.jp/seiga/${s}`);
-						else if (s.startsWith("lv")) redirect(`https://live.nicovideo.jp/watch/${s}`);
-						else if (s.startsWith("mg")) redirect(`https://seiga.nicovideo.jp/watch/${s}`);
-						else if (s.startsWith("nw")) redirect(`https://news.nicovideo.jp/watch/${s}`);
-						else if (s.startsWith("so")) redirect(`https://www.nicovideo.jp/watch/${s}`);
-						break;
+						else if (s.startsWith("sm")) redirect(`https://www.nicovideo.jp/watch/${s}`)
+						else if (s.startsWith("ch")) redirect(`https://ch.nicovideo.jp/channel/${s}`)
+						else if (s.startsWith("im")) redirect(`https://seiga.nicovideo.jp/seiga/${s}`)
+						else if (s.startsWith("lv")) redirect(`https://live.nicovideo.jp/watch/${s}`)
+						else if (s.startsWith("mg")) redirect(`https://seiga.nicovideo.jp/watch/${s}`)
+						else if (s.startsWith("nw")) redirect(`https://news.nicovideo.jp/watch/${s}`)
+						else if (s.startsWith("so")) redirect(`https://www.nicovideo.jp/watch/${s}`)
+						break
 					case "nj":
-						redirect(`https://nijie.info/view.php?id=${s}`);
-						break;
+						redirect(`https://nijie.info/view.php?id=${s}`)
+						break
 					case "p":
 					case "pa":
-						redirect(`https://www.pixiv.net/artworks/${s}`);
-						break;
+						redirect(`https://www.pixiv.net/artworks/${s}`)
+						break
 					case "pu":
-						redirect(`https://www.pixiv.net/users/${s}`);
-						break;
+						redirect(`https://www.pixiv.net/users/${s}`)
+						break
 					case "t":
 					case "ts":
-						redirect(`https://twitter.com/i/status/${s}`);
-						break;
+						redirect(`https://twitter.com/i/status/${s}`)
+						break
 					case "ti":
-						redirect(`https://pbs.twimg.com/media/${s}?format=jpg&name=large`);
-						break;
+						redirect(`https://pbs.twimg.com/media/${s}?format=jpg&name=large`)
+						break
 					case "w":
-						redirect(`https://m.weibo.cn/status/${s}`);
-						break;
+						redirect(`https://m.weibo.cn/status/${s}`)
+						break
 					case "y":
 					case "yp":
-						redirect(`https://yande.re/post/show/${s}`);
-						break;
+						redirect(`https://yande.re/post/show/${s}`)
+						break
 					case "yt":
-						redirect(`https://youtu.be/${s}`);
-						break;
+						redirect(`https://youtu.be/${s}`)
+						break
 					default:
-						console.log("Unknown parameter.");
+						console.log("Unknown parameter.")
 				}
-				break;
+				break
 			default:
-				console.log("Nothing to do.");
+				console.log("Nothing to do.")
 		}
 	}
 }
-var quiet = true;
-hash_func();
-window.addEventListener("hashchange", hash_func, false);
+var quiet = true
+hash_func()
+window.addEventListener("hashchange", hash_func, false)
